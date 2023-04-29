6049a24f767bcd6f252e14ff84036125e04b03f7
JP <joaopaulolopesbrito@gmail.com>
2023-04-29 20:13:14 -0300

    Header Home; added styled components lib

---
+++ styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components/native";
+
+export const Header = styled.View`
+flex-direction: row;
+width: 100%;
+height: 64px;
+justify-content: flex-start;
+align-items: center;
+background: #000421;
+padding: 0 8px;
+`;
+
+export const Logo = styled.Text`
+color: #fff;
+font-size: 32px;
+font-weight: 600;
+`;
+
+export const Body = styled.View`
+
+`;
